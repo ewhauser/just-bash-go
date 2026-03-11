b15d8bfe2d5e69ae495b5d0562b1c590c8c11e84
Eric Hauser <ewhauser@gmail.com>
2026-03-11 15:13:43 -0600

    ci: fix benchmark and windows snapshot checks

--- fs/host_windows.go
+++ fs/host_windows.go
@@ -2,7 +2,12 @@
 
 package fs
 
-import "errors"
+import (
+	"context"
+	"errors"
+	stdfs "io/fs"
+	"time"
+)
 
 var errHostUnsupported = errors.New("host-backed filesystem is unsupported on Windows")
 
@@ -11,5 +16,53 @@ type HostFS struct{}
 
 // NewHost returns an unsupported error on Windows.
 func NewHost(HostOptions) (*HostFS, error) {
-	return nil, errHostUnsupported
+	return nil, unsupportedHostError()
 }
+
+func (HostFS) Open(context.Context, string) (File, error) { return nil, unsupportedHostError() }
+
+func (HostFS) OpenFile(context.Context, string, int, stdfs.FileMode) (File, error) {
+	return nil, unsupportedHostError()
+}
+
+func (HostFS) Stat(context.Context, string) (stdfs.FileInfo, error) {
+	return nil, unsupportedHostError()
+}
+
+func (HostFS) Lstat(context.Context, string) (stdfs.FileInfo, error) {
+	return nil, unsupportedHostError()
+}
+
+func (HostFS) ReadDir(context.Context, string) ([]stdfs.DirEntry, error) {
+	return nil, unsupportedHostError()
+}
+
+func (HostFS) Readlink(context.Context, string) (string, error) { return "", unsupportedHostError() }
+
+func (HostFS) Realpath(context.Context, string) (string, error) { return "", unsupportedHostError() }
+
+func (HostFS) Symlink(context.Context, string, string) error { return unsupportedHostError() }
+
+func (HostFS) Link(context.Context, string, string) error { return unsupportedHostError() }
+
+func (HostFS) Chmod(context.Context, string, stdfs.FileMode) error { return unsupportedHostError() }
+
+func (HostFS) Chtimes(context.Context, string, time.Time, time.Time) error {
+	return unsupportedHostError()
+}
+
+func (HostFS) MkdirAll(context.Context, string, stdfs.FileMode) error { return unsupportedHostError() }
+
+func (HostFS) Remove(context.Context, string, bool) error { return unsupportedHostError() }
+
+func (HostFS) Rename(context.Context, string, string) error { return unsupportedHostError() }
+
+func (HostFS) Getwd() string { return "/" }
+
+func (HostFS) Chdir(string) error { return unsupportedHostError() }
+
+func unsupportedHostError() error {
+	return &stdfs.PathError{Op: "hostfs", Path: "/", Err: errHostUnsupported}
+}
+
+var _ FileSystem = (*HostFS)(nil)
--- internal/compatfs/hostfs_windows.go
+++ internal/compatfs/hostfs_windows.go
@@ -2,7 +2,14 @@
 
 package compatfs
 
-import "errors"
+import (
+	"context"
+	"errors"
+	stdfs "io/fs"
+	"time"
+
+	jbfs "github.com/ewhauser/jbgo/fs"
+)
 
 var errUnsupported = errors.New("host compatibility filesystem is unsupported on Windows")
 
@@ -11,3 +18,45 @@ type HostFS struct{}
 func New() (*HostFS, error) {
 	return nil, errUnsupported
 }
+
+func (HostFS) Open(context.Context, string) (jbfs.File, error) { return nil, unsupportedError() }
+
+func (HostFS) OpenFile(context.Context, string, int, stdfs.FileMode) (jbfs.File, error) {
+	return nil, unsupportedError()
+}
+
+func (HostFS) Stat(context.Context, string) (stdfs.FileInfo, error) { return nil, unsupportedError() }
+
+func (HostFS) Lstat(context.Context, string) (stdfs.FileInfo, error) { return nil, unsupportedError() }
+
+func (HostFS) ReadDir(context.Context, string) ([]stdfs.DirEntry, error) {
+	return nil, unsupportedError()
+}
+
+func (HostFS) Readlink(context.Context, string) (string, error) { return "", unsupportedError() }
+
+func (HostFS) Realpath(context.Context, string) (string, error) { return "", unsupportedError() }
+
+func (HostFS) Symlink(context.Context, string, string) error { return unsupportedError() }
+
+func (HostFS) Link(context.Context, string, string) error { return unsupportedError() }
+
+func (HostFS) Chmod(context.Context, string, stdfs.FileMode) error { return unsupportedError() }
+
+func (HostFS) Chtimes(context.Context, string, time.Time, time.Time) error { return unsupportedError() }
+
+func (HostFS) MkdirAll(context.Context, string, stdfs.FileMode) error { return unsupportedError() }
+
+func (HostFS) Remove(context.Context, string, bool) error { return unsupportedError() }
+
+func (HostFS) Rename(context.Context, string, string) error { return unsupportedError() }
+
+func (HostFS) Getwd() string { return "/" }
+
+func (HostFS) Chdir(string) error { return unsupportedError() }
+
+func unsupportedError() error {
+	return &stdfs.PathError{Op: "compatfs", Path: "/", Err: errUnsupported}
+}
+
+var _ jbfs.FileSystem = (*HostFS)(nil)
