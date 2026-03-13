6362c8ec7bf26c683a0aadd7f2979399a5d06660
Eric Hauser <ewhauser@gmail.com>
2026-03-13 16:18:46 -0600

    Migrate whoami to command spec parser

--- commands/whoami.go
+++ commands/whoami.go
@@ -3,6 +3,7 @@ package commands
 import (
 	"context"
 	"fmt"
+	"io"
 	"strings"
 )
 
@@ -16,25 +17,46 @@ func (c *Whoami) Name() string {
 	return "whoami"
 }
 
-func (c *Whoami) Run(_ context.Context, inv *Invocation) error {
-	args := append([]string(nil), inv.Args...)
-	for _, arg := range args {
-		switch {
-		case arg == "--help":
-			_, _ = fmt.Fprintln(inv.Stdout, "usage: whoami")
-			return nil
-		case arg == "--version":
-			_, _ = fmt.Fprintln(inv.Stdout, "whoami (gbash)")
-			return nil
-		case strings.HasPrefix(arg, "--"):
-			return exitf(inv, 1, "whoami: unrecognized option '%s'", arg)
-		case strings.HasPrefix(arg, "-"):
-			return exitf(inv, 1, "whoami: invalid option -- '%s'", strings.TrimPrefix(arg, "-"))
-		default:
-			return exitf(inv, 1, "whoami: extra operand '%s'\nTry 'whoami --help' for more information.", arg)
-		}
-	}
+func (c *Whoami) Run(ctx context.Context, inv *Invocation) error {
+	return RunCommand(ctx, c, inv)
+}
 
+func (c *Whoami) Spec() CommandSpec {
+	return CommandSpec{
+		Name:  "whoami",
+		Usage: "whoami",
+		HelpRenderer: func(w io.Writer, _ CommandSpec) error {
+			_, err := io.WriteString(w, "usage: whoami\n")
+			return err
+		},
+		Args: []ArgSpec{
+			{Name: "args", ValueName: "ARG", Repeatable: true},
+		},
+		Parse: ParseConfig{
+			InferLongOptions: true,
+			AutoHelp:         true,
+			AutoVersion:      true,
+		},
+	}
+}
+
+func (c *Whoami) NormalizeParseError(inv *Invocation, err error) error {
+	if err == nil {
+		return nil
+	}
+	msg := err.Error()
+	trimmed := strings.TrimSuffix(msg, "Try 'whoami --help' for more information.\n")
+	trimmed = strings.TrimSuffix(trimmed, "Try 'whoami --help' for more information.")
+	if trimmed != msg && (strings.Contains(trimmed, "unrecognized option") || strings.Contains(trimmed, "invalid option --")) {
+		return exitf(inv, 1, "%s", strings.TrimRight(trimmed, "\n"))
+	}
+	return err
+}
+
+func (c *Whoami) RunParsed(_ context.Context, inv *Invocation, matches *ParsedCommand) error {
+	if args := matches.Args("args"); len(args) > 0 {
+		return exitf(inv, 1, "whoami: extra operand %s\nTry 'whoami --help' for more information.", quoteGNUOperand(args[0]))
+	}
 	_, err := fmt.Fprintln(inv.Stdout, idCurrentIdentity(inv).userName)
 	if err != nil {
 		return &ExitError{Code: 1, Err: err}
@@ -43,3 +65,6 @@ func (c *Whoami) Run(_ context.Context, inv *Invocation) error {
 }
 
 var _ Command = (*Whoami)(nil)
+var _ SpecProvider = (*Whoami)(nil)
+var _ ParsedRunner = (*Whoami)(nil)
+var _ ParseErrorNormalizer = (*Whoami)(nil)
